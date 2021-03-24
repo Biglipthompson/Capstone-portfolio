60de169609b5c42804e14666ab4b562afd7bf604
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-03-23 23:45:30 -0400

    Commit
    Updating my website.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const alertBanner = document.getElementById("alert");
+// const alertBanner = document.getElementById("alert");
 
-alertBanner.innerHTML = `
-<div class="alert alert-info bg-info text-light text-center" role="alert">
-<p p class="alert-banner-close bg-dark">X</p><p class="alert-text">Welcome To My Personal Website!</p></p> 
-</div>
-`;
-alertBanner.addEventListener('click', (e) => {
-    const element = e.target;
-    if (element.classList.contains("alert-banner-close")) {
-      alertBanner.style.display = 'none';
-    }
-  });
+// alertBanner.innerHTML = `
+// <div class="alert alert-info bg-info text-light text-center" role="alert">
+// <p p class="alert-banner-close bg-dark">X</p><p class="alert-text">Welcome To My Personal Website!</p></p> 
+// </div>
+// `;
+// alertBanner.addEventListener('click', (e) => {
+//     const element = e.target;
+//     if (element.classList.contains("alert-banner-close")) {
+//       alertBanner.style.display = 'none';
+//     }
+//   });
+
